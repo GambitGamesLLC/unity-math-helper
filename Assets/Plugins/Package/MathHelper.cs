c510c8fd9ce70ba3a867974c31a98a25bb84231c
Derrick Barra <gambitgamesllc@gmail.com>
2025-06-16 10:06:59 -0400

    Update MathHelper.cs

--- Assets/Plugins/Package/MathHelper.cs
+++ Assets/Plugins/Package/MathHelper.cs
@@ -13,30 +13,45 @@ namespace gambit.mathhelper
     /// </summary>
     public static class MathHelper
     {
+
+        #region PUBLIC - MAP
+
         /// <summary>
         /// Remaps a value from one range to another.
         /// </summary>
         /// <param name="value">The input value to remap.</param>
-        /// <param name="inMin">The original range's minimum.</param>
-        /// <param name="inMax">The original range's maximum.</param>
-        /// <param name="outMin">The new range's minimum.</param>
-        /// <param name="outMax">The new range's maximum.</param>
+        /// <param name="fromMin">The original range's minimum.</param>
+        /// <param name="fromMax">The original range's maximum.</param>
+        /// <param name="toMin">The new range's minimum.</param>
+        /// <param name="toMax">The new range's maximum.</param>
         /// <returns>The remapped value in the target range.</returns>
         //--------------------------------------------------------------------------------------------//
-        public static float Map(float value, float inMin, float inMax, float outMin, float outMax)
+        public static float Map(float value, float fromMin, float fromMax, float toMin, float toMax)
         //--------------------------------------------------------------------------------------------//
         {
-            if (Mathf.Approximately(inMax - inMin, 0f))
+            if(fromMin == fromMax)
             {
-                Debug.LogWarning("[MathHelper] Input range is zero. Returning outMin.");
-                return outMin;
+                Debug.LogError( "MathHelper.cs Map() fromMin & fromMax are equal, returning the 'toMin' value" );
+                return toMin;
+            }
+            if(toMin == toMax)
+            {
+                Debug.LogError( "MathHelper.cs Map() toMin & toMax are equal, returning the 'toMin' value" );
+                return toMin;
+            }
+            if(fromMin == toMin && fromMax == toMax)
+            {
+                Debug.LogError( "MathHelper.cs Map() passed in 'fromMin' is the same as 'toMin', and 'fromMax' is the same as 'toMax'. Unable to continue" );
+                return toMin;
             }
 
-            float normalized = (value - inMin) / (inMax - inMin);
-            return outMin + (normalized * (outMax - outMin));
+            //Perform the remapping
+            return ((value - fromMin) * (toMax - toMin) / (fromMax - fromMin) + toMin);
 
-        } //END Map
-    
+        } //END Map Method
+
+        #endregion
+
     } //END MathHelper class
 
 } //END gambit.mathhelper namespace
